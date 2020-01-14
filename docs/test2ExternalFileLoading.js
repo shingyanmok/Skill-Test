4376a0c9be0daf5032ced5174a813cc07adeea8e
shingyanmok <56969659+shingyanmok@users.noreply.github.com>
2020-01-14 16:37:49 +0800

    Add files via upload

--- docs/test2ExternalFileLoading.js
+++ docs/test2ExternalFileLoading.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	var catagory = [];
 
    $.getJSON('test2JsonExample.json', function(data) {
-       $.each(catagory, function(i, f) {
+       $.each(data, function(i, f) {
           var tblRow = "<tr>" + "<td>" + f.cd.title + "</td>" + "<td>" + f.cd.artist + "</td>" + "<td>" + f.cd.country + "</td>" + "<td>" + f.cd.company + "</td>" + "</tr>"
            $(tblRow).appendTo("#externalJson table");
      });
