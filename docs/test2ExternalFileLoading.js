190aca3a3e403aa2f9d323240532765ba771933a
shingyanmok <56969659+shingyanmok@users.noreply.github.com>
2020-01-14 17:07:10 +0800

    Add files via upload

--- docs/test2ExternalFileLoading.js
+++ docs/test2ExternalFileLoading.js
@@ -2,13 +2,13 @@ $(document).ready(function(){
 	var catagory = [];
 
 	const test="<p>Test ver 1.0</p>";
-   $(test).appendTo("#test div");
+	$(test).appendTo("#test div");
 
-   $.getJSON('test2JsonExample.json', function(data) {
-       $.each(data, function(i, f) {
-          var tblRow = "<tr>" + "<td>" + f.title + "</td>" + "<td>" + f.artist + "</td>" + "<td>" + f.country + "</td>" + "<td>" + f.company + "</td>" + "</tr>"
-           $(tblRow).appendTo("#externalJson table");
-     });
+	$.getJSON('test2JsonExample.json', function(data) {
+		$.each(data.catagory.cd , function(i, f) {
+			var tblRow = "<tr>" + "<td>" + f.title + "</td>" + "<td>" + f.artist + "</td>" + "<td>" + f.country + "</td>" + "<td>" + f.company + "</td>" + "</tr>"
+			$(tblRow).appendTo("#externalJson tbody");
+		});
 
-   });
+	});
 });
